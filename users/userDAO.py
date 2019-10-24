f7069fb8f1eb87c51ae3e5e66292b11ad4668c50
xuxvming <oliverxiu18@gmail.com>
2019-10-24 14:48:14 +0100

    Hosting MongoDB on GCP

--- users/userDAO.py
+++ users/userDAO.py
@@ -1,15 +1,17 @@
 from pymongo import MongoClient
 
+from settings import MONGO_DB_CONNECTION
+
+
 class UserDAO:
 
-    def __init__(self):
-        client = MongoClient()
+    client = MongoClient(MONGO_DB_CONNECTION)
 
     def get_database(self, database_name='user_db'):
         return self.client[database_name]
 
     def insert_document(self, user_data, collection):
-        db = self.get_database(self.client)
+        db = self.get_database()
         collection = db[collection]
         inserted_id = collection.insert_one(user_data).inserted_id
         return inserted_id
